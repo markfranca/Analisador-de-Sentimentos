0476476d496cdce6afb98c950db296096c50d7a5
Marcus Vinicius <mvfranca2004@gmail.com>
2025-09-03 19:55:35 -0300

    feat: put id,date and origin to sqlite database

--- src/database.py
+++ src/database.py
@@ -6,5 +6,19 @@ con = sqlite3.connect('database.db')
 cur = con.cursor()
 
 
-df = pd.read_csv('IMDB Dataset.csv')
+
+
+df = pd.read_csv(r'C:\Users\MARCUS VINICIUS\Documents\analisador-sentimento\data\IMDB Dataset.csv')
+
+
+df.drop(columns=["sentiment"], inplace=True)
+
+
+df["id"] = range(1, len(df) + 1)
+df["date"] = pd.Timestamp.now().normalize()
+df["origin"] = "IMDB Dataset"
+print(df.head())
+
+
+
 df.to_sql('reviews', con, if_exists='replace', index=False)
